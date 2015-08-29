c4871ed0095a59b03d01073e7da845fadbd9def6
fajarekos <fajarekosusilo10@gmail.com>
2015-08-29 13:02:01 +0700

    nama file singo controller

---
+++ application/controllers/singo.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Singo extends CI_Controller {
+
+	public function index()
+	{
+		echo "nyoba push";
+	}
+
+}
+
+/* End of file singo.php */
+/* Location: ./application/controllers/singo.php */
