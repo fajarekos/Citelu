7ab39b688d020518c2b4747d45e7601fcb102a4d
Rahendra Putra K <rahendz@gmail.com>
2015-08-29 13:18:27 +0700

    nambahi file telo ning controller

---
+++ application/controllers/telo.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Telo extends CI_Controller {
+
+	public function index()
+	{
+		echo 'aku nambah file';
+	}
+
+}
+
+/* End of file telo.php */
+/* Location: ./application/controllers/telo.php */
